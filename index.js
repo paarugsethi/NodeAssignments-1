c618cd3a900283e33c2c3d99ce8ecf4e49ddb0d2
paarugsethi <paarugsethi@gmal.com>
2022-01-26 22:16:42 +0530

    Sum commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-        data: "Hello World!"
-    }));
-})
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     res.end(JSON.stringify({
+//         data: "Hello World!"
+//     }));
+// })
 
-server.listen(3001)
+// server.listen(3001)
+
+const sum = require("./sum.js");
+
+console.log(sum(10,5));
