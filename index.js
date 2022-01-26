aa84fed9ca48b9d32b9f56d72cb609336a8c7a23
paarugsethi <paarugsethi@gmal.com>
2022-01-27 01:03:21 +0530

    Push, get, pagination commit

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-// const http = require('http');
+const http = require('http');
 
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type': 'application/json'});
-//     res.end(JSON.stringify({
-//         data: "Hello World!"
-//     }));
-// })
+const users = ["Paarug", "Simran","Namo","Aayush","Sudarsh","Shruti","Vanshika","Sanyam","Aakash","Kunal"]
 
-// server.listen(3001)
+function getAllUsers(page=1){
+    const start = (page - 1) * 3;
+    const end = start + 3;
+    return users.slice(start, end);
+}
 
-const sum = require("./sum.js");
+function getUsersById(index){
+    if (index >= 0 && index < users.length){
+        return users[index]
+    }
+    else{
+        return null
+    }
+}
 
-console.log(sum(10,5));
+function addUser (name) {
+    users.push(name);
+    return users;
+}
+
+const server = http.createServer((req, res) => {
+    try{
+        const [url, query] = req.url.split("?");
+        if (url === '/users'){
+            if (req.method === "GET"){
+                const q = new URLSearchParams(`?${query}`);
+                const page = q.get('page') ?? 1;
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(getAllUsers(Number(page))));
+            }
+            else if (req.method === "POST"){
+                const q = new URLSearchParams(`?${query}`);
+                const name = q.get('name');
+                addUser(name);
+                res.writeHead(201, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(getAllUsers()));
+            }
+        }
+        else if (url.startsWith('/users/')){
+            const index = Number(url.split('/')[2])
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(getUsersById(index)));
+        }
+        else{
+            throw new Error("Something went wrong")
+        }
+    }
+    catch{
+        res.writeHead(500, {'Content-Type': 'application/json'});
+    }
+    
+})
+
+server.listen(3001)
+
+// const sum = require("./sum.js");
+
+// console.log(sum(10,5));
